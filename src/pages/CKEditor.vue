013f70fc4bb39780585f57eb0ce7804a1a84a9e5
mojung <mojung@ipactory.com>
2023-01-04 15:33:24 +0900

    fix: CKEDitor router 설정

---
+++ src/pages/CKEditor.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <ckeditor :editor="editor" v-model="editorData" :config="editorConfig" />
+      <br>
+      <hr>
+      <br>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import CKEditor from '@ckeditor/ckeditor5-vue';
+  import Editor from 'ckeditor5-custom-build';
+  import { ref } from 'vue';
+  
+  const ckeditor = CKEditor.component;
+  const editor = Editor;
+  const editorData = ref('');
+  const editorConfig = '<p>Content of the editor.</p>';
+  </script>
+  
+  <style scoped></style>
+  
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,18 +1,49 @@
 <template>
-  <div>
-    <ckeditor :editor="editor" v-model="editorData" :config="editorConfig" />
-  </div>
+  <q-page class="row items-center justify-evenly">
+    <example-component
+      title="Example component"
+      active
+      :todos="todos"
+      :meta="meta"
+    ></example-component>
+  </q-page>
 </template>
 
-<script setup lang="ts">
-import CKEditor from '@ckeditor/ckeditor5-vue';
-import Editor from 'ckeditor5-custom-build';
-import { ref } from 'vue';
+<script lang="ts">
+import { Todo, Meta } from 'components/models';
+import ExampleComponent from 'components/ExampleComponent.vue';
+import { defineComponent, ref } from 'vue';
 
-const ckeditor = CKEditor.component;
-const editor = Editor;
-const editorData = ref('');
-const editorConfig = '<p>Content of the editor.</p>';
+export default defineComponent({
+  name: 'IndexPage',
+  components: { ExampleComponent },
+  setup() {
+    const todos = ref<Todo[]>([
+      {
+        id: 1,
+        content: 'ct1',
+      },
+      {
+        id: 2,
+        content: 'ct2',
+      },
+      {
+        id: 3,
+        content: 'ct3',
+      },
+      {
+        id: 4,
+        content: 'ct4',
+      },
+      {
+        id: 5,
+        content: 'ct5',
+      },
+    ]);
+    const meta = ref<Meta>({
+      totalCount: 1200,
+    });
+    return { todos, meta };
+  },
+});
 </script>
-
-<style scoped></style>
